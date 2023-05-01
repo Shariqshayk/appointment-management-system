67108c49614b0b34d289971d1a39eefabb7b38bd
OGL0K <oguzgokyuzu01@gmail.com>
2023-05-01 14:45:47 +0100

    Vue Project Initiated

---
+++ src/main/UI/frontend-medimeet/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import Home from './Home.vue'
+export default {
+  components: {
+    Home
+  }
+}
+</script>
+<template>
+  <Home />
+</template>
+<style scoped>
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+@media (min-width: 1024px) {
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+}
+</style>
---
+++ src/main/UI/frontend-medimeet/src/Home.vue
@@ -0,0 +1,8 @@
+<template>
+      <header>
+            <div id="app">
+            <img src="./assets/medimeet.png" height="120">
+            </div>
+            <h1 id="header">Welcome to MediMeet!</h1>
+      </header>      
+</template>
